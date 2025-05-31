3660a9500af34a30ae50a8946f5726dc739118cd
Poikilos <7557867+poikilos@users.noreply.github.com>
2025-05-31 14:33:54 -0400

    Move example to a separate folder.

--- TouchGFX/build/MINGW32_NT-6.2/generated/images/src/__generated/image_icon_theme_images_action_all_inbox_60_60_000000.svg.d
+++
@@ -1,5 +0,0 @@
-build/MINGW32_NT-6.2/generated/images/src/__generated/image_icon_theme_images_action_all_inbox_60_60_000000.svg.o: \
- generated/images/src/__generated/image_icon_theme_images_action_all_inbox_60_60_000000.svg.cpp \
- ../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-
-../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
